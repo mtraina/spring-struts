88990f5a49fdc9b88c2efc8c1d3110799ea08c08
mtraina <Matteo_Traina@epchzurw0012.budapest.epam.com>
2016-06-16 16:59:23 +0200

    new author

---
+++ src/main/java/com/mtraina/common/form/HelloWorldForm.java
@@ -0,0 +1,15 @@
+package com.mtraina.common.form;
+
+import org.apache.struts.action.ActionForm;
+
+public class HelloWorldForm extends ActionForm {
+	String message;
+	
+	public String getMessage(){
+		return message;
+	}
+	
+	public void setMessage(String message){
+		this.message = message;
+	}
+}
